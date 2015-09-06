83755e9f9498048e9594465fe478dd0551dcb57c
Spencer Ong <spencer.pg@gmail.com>
2015-09-06 14:05:38 +0100

    Add files.

---
+++ flaskapp.py
@@ -0,0 +1,18 @@
+"""
+The flask application package.
+"""
+
+from flask import request
+from flask import Response
+import flask
+import urllib.request as web
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def home():
+	url = request.form['a']
+	bla = web.urlopen(url).readall().decode('utf-8')
+	return Response(response=bla, status=200, mimetype="application/json")
+
+
---
+++ wsgi.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+
+from flaskapp import app as application
+
+#
+# Below for testing only
+#
+if __name__ == '__main__':
+    #from wsgiref.simple_server import make_server
+    #httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    #httpd.serve_forever()
+	application.debug=True
+	application.run()
