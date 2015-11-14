2c669fa8d0fbe1e965c128313b7bcf66cf866819
Spencer Ong <spencer.pg@gmail.com>
2015-11-14 12:48:35 +0000

    - Fixed indentation bug in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -8,4 +8,4 @@ from flaskapp import app as application
 if __name__ == '__main__':
 	application.debug=True
 	port = int(os.environ.get("PORT", 5000))
-    application.run(host='0.0.0.0', port=port)
+	application.run(host='0.0.0.0', port=port)
